e17cc5936631b0ca1346155ab0125e8bfbb25a48
rahulmayuran <rahulmayuran1996@gmail.com>
2021-07-15 20:08:04 +0530

    Added the methods to repository, service and controller as well

--- src/main/java/com/flight/app/Repository/FlightRepository.java
+++ src/main/java/com/flight/app/Repository/FlightRepository.java
@@ -2,6 +2,9 @@ package com.flight.app.Repository;
 
 import com.flight.app.Model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public abstract class FlightRepository implements JpaRepository<Flight, Integer> {
+
 }
--- src/main/java/com/flight/app/controller/FlightController.java
+++ src/main/java/com/flight/app/controller/FlightController.java
@@ -1,5 +1,8 @@
 package com.flight.app.controller;
 
+import com.flight.app.Model.Flight;
+import com.flight.app.service.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +12,9 @@ import java.util.List;
 @RestController
 public class FlightController {
 
+    @Autowired
+    FlightService flightService;
+
     @GetMapping("/home")
     public String greet(){
         return "Welcome to Flight App";
@@ -17,5 +23,10 @@ public class FlightController {
     @GetMapping("/flightList")
     public List<String> flightList(){
         return Arrays.asList("AirIndia","Go-Air","Indigo");
+    }
+
+    @GetMapping("/flightList")
+    public Flight getFlightById(int id){
+        return flightService.getFLightByFlightId(id);
     }
 }
