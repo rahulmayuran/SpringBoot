137b94a10e880a37428ec009e25774f515761bb1
rahulmayuran <rahulmayuran1996@gmail.com>
2021-07-15 22:11:16 +0530

    Added test code, whiteLabel handler and some other stuff

--- src/main/java/com/flight/app/Main/FlightApplication.java
+++ src/main/java/com/flight/app/Main/FlightApplication.java
@@ -3,14 +3,28 @@ package com.flight.app.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-public class FlightApplication {
+public class FlightApplication{
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(FlightApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder templateBuilder){
+
+		return templateBuilder
+				.setConnectTimeout(Duration.ofMillis(3000))
+				.setReadTimeout(Duration.ofMillis(3000))
+				.build();
 	}
 
 }
