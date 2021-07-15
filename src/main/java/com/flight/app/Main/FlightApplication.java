d6a64365a302b04986493006dc837980faf2f9db
rahulmayuran <rahulmayuran1996@gmail.com>
2021-07-15 22:36:58 +0530

    Added @EnableEurekaClient to pom and used the annotation in SpringBootApp

--- src/main/java/com/flight/app/Main/FlightApplication.java
+++ src/main/java/com/flight/app/Main/FlightApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ import java.time.Duration;
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableEurekaClient
 public class FlightApplication{
 
 	public static void main(String[] args) {
