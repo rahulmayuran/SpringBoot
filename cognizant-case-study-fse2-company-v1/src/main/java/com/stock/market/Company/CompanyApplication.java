7adf3f3f8b4c77bbc1cf4e7357b001a807e920a3
rahulmayuran <rahulmayuran1996@gmail.com>
2022-02-09 22:03:25 +0530

    Added kafka properties to Company Service

--- cognizant-case-study-fse2-company-v1/src/main/java/com/stock/market/Company/CompanyApplication.java
+++ cognizant-case-study-fse2-company-v1/src/main/java/com/stock/market/Company/CompanyApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -12,6 +13,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableEurekaClient
 @EnableCaching
 @EnableMongoRepositories
+@ConfigurationPropertiesScan
 public class CompanyApplication implements CommandLineRunner {
 
 	@Value(value = "${environment}")
--- cognizant-case-study-fse2-company-v1/src/main/java/com/stock/market/Company/Controller/CompanyController.java
+++ cognizant-case-study-fse2-company-v1/src/main/java/com/stock/market/Company/Controller/CompanyController.java
@@ -1,10 +1,13 @@
 package com.stock.market.Company.Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stock.market.Company.Model.Company;
 import com.stock.market.Company.Service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 import java.util.Comparator;
 import java.util.List;
@@ -18,9 +21,15 @@ public class CompanyController {
 
     @Autowired
     CompanyService companyService;
+    
+    @Autowired
+    private ObjectMapper mapper;
+    
+    @Autowired
+    private KafkaTemplate<String, String> kakfaTemplate;
 
     @PostMapping("/company/register")
-    public ResponseEntity<Company> addCompany(@RequestBody Company company) 
+    public ResponseEntity<Company> addCompany(@RequestBody Company company) throws JsonProcessingException 
     {
     	UUID randomUUID = UUID.randomUUID();
 
@@ -35,21 +44,27 @@ public class CompanyController {
     		company.setCompanyId(1);
         	company.setCompanyCode(randomUUID);   
         	Company addnewCompany = companyService.addCompany(company);
-            System.out.println("Company added "+addnewCompany);
+            System.out.println("Company added "+ mapper.writeValueAsString(addnewCompany));
+            kakfaTemplate.send("fse_company", "***Adding First Company***");
+            kakfaTemplate.send("fse_company", mapper.writeValueAsString(addnewCompany));
             return new ResponseEntity<>(addnewCompany, HttpStatus.OK); 
     	}
     	else {
         	company.setCompanyId(maxId+1);
         	company.setCompanyCode(randomUUID);   
         	Company addotherCompany = companyService.addCompany(company);
-        	System.out.println("Company added "+addotherCompany);
+        	System.out.println("Company added "+ mapper.writeValueAsString(addotherCompany));
+        	kakfaTemplate.send("fse_company", "***Adding Company*** -> "+company.getCompanyName());
+            kakfaTemplate.send("fse_company", mapper.writeValueAsString(addotherCompany));
             return new ResponseEntity<>(addotherCompany, HttpStatus.OK);
     	}
     }
     
     @DeleteMapping("/company/delete/{id}")
-    public String deleteCompany(@PathVariable int id){
+    public String deleteCompany(@PathVariable int id) throws JsonProcessingException{
         companyService.deleteCompany(id);
+        kakfaTemplate.send("fse_company", "***Deleted Company***");
+        kakfaTemplate.send("fse_company", mapper.writeValueAsString(id));
         return "Company with id - "+id+" deleted";
     }
 
