ed0eb424c7d97f2f021cf853a501e82a7cf81c4f
rahulmayuran <rahulmayuran1996@gmail.com>
2022-03-11 16:29:45 +0530

    Added Dockerfiles in services and pushed to ACR's and CI of azure

--- cognizant-case-study-fse2-eureka-server-v1/src/main/java/com/eureka/EurekaApplication.java
+++ cognizant-case-study-fse2-eureka-server-v1/src/main/java/com/eureka/EurekaApplication.java
@@ -1,5 +1,6 @@
 package com.eureka;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -11,5 +12,7 @@ public class EurekaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
 	}
-
+	
+	@Value("${environment}")
+	String key;
 }
