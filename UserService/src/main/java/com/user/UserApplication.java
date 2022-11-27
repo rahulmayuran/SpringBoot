9e7af77e31f3cfd07645aebd01dbbfac4ea2ba05
rahulmayuran <rahulmayuran1996@gmail.com>
2022-11-27 13:40:18 +0530

    Code clean-up

--- UserService/src/main/java/com/user/UserApplication.java
+++ UserService/src/main/java/com/user/UserApplication.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
