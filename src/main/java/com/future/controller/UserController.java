2e7595945760e90407daa07daeda46b698585db6
rahulmayuran <rahulmayuran1996@gmail.com>
2023-09-19 01:02:11 +0530

    Added MultiThreading using CompletableFuture

--- src/main/java/com/future/controller/UserController.java
+++ src/main/java/com/future/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
