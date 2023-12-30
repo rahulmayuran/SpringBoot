ab28fd9ef825397053917fa545923a042d812ebe
rahulmayuran <rahulmayuran1996@gmail.com>
2023-12-30 23:03:11 +0530

    Added Caching to existing code - Need to test it

--- src/main/java/com/future/cache/UserCacheLoader.java
+++ src/main/java/com/future/cache/UserCacheLoader.java
@@ -1,6 +1,7 @@
 package com.future.cache;
 
 import com.future.dto.UserDto;
+import com.future.repository.UserRepository;
 import com.google.common.cache.CacheLoader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +12,32 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UserCacheLoader extends CacheLoader<String, UserDto> {
 
+    private final UserRepository repository;
 
     @Override
-    public UserDto load(String key) throws Exception {
-        return null;
+    public UserDto load(String userId) {
+
+        log.info("UserCacheLoader :: load() method call");
+        if(null != userId){
+            //Cache the userDto object using the id
+            UserDto userDto = repository.getById(Integer.valueOf(userId)).copytoUserDto();
+            if(null != userDto){
+                return userDto;
+            }
+        }
+        return UserDto.builder().build();
     }
+
+/*
+    @Override
+    public Map<String, UserDto> loadAll(Iterable<? extends String> keys) throws Exception {
+
+        Map<String, List<User>> loadedEntries = new HashMap<>();
+        List<User> userList = repository.findAll();
+        for (String key : keys) {
+            loadedEntries.put(key, userList);
+        }
+        log.info("UserCacheLoader :: load() method call, Cached the List of users using");
+        return loadedEntries;
+    }*/
 }
--- src/main/java/com/future/dto/UserDto.java
+++ src/main/java/com/future/dto/UserDto.java
@@ -3,6 +3,7 @@ package com.future.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.future.entity.User;
 import com.future.utils.CopyUtility;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
 
